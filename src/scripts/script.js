4cf357d383dcdf642faad49bbfff6f2e0503cc21
William Almeida <willliam.dev@gmail.com>
2024-01-18 22:39:56 -0300

    create a real stop function and refact the pause funtion

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,31 +1,45 @@
 const timer = document.querySelector(".timer")
 const startButton = document.querySelector("#start")
 const stopButton = document.querySelector("#stop")
+const pauseButton = document.querySelector("#pause")
 
 startButton.addEventListener("click", startTimer)
-
+pauseButton.addEventListener("click", pauseTimer)
 stopButton.addEventListener("click", stopTimer)
 
 let time = 0
 let intervalId
 
 function startTimer() {
-  stopButton.classList.remove("hide")
+  startButton.innerHTML = "Resume"
+  pauseButton.classList.remove("hide")
+  stopButton.classList.add("hide")
   startButton.classList.add("hide")
   if (!intervalId) {
-    intervalId = setInterval(soma, 1)
+    intervalId = setInterval(soma, 10)
   }
 }
 
-function stopTimer() {
+function pauseTimer() {
+  pauseButton.classList.add("hide")
   startButton.classList.remove("hide")
-  stopButton.classList.add("hide")
+  stopButton.classList.remove("hide")
+
   clearInterval(intervalId)
   intervalId = null
 }
 
+function stopTimer() {
+  startButton.innerHTML = "Start"
+  startButton.classList.remove("hide")
+  stopButton.classList.add("hide")
+  pauseButton.classList.add("hide")
+  timer.innerHTML = "00:00"
+  clearInterval(intervalId)
+  intervalId = null
+  time = 0
+}
 function soma() {
-  console.log(intervalId)
   time += 1
   let seconds = String(time % 60).padStart(2, "0")
   let minutes = String(Math.floor(time / 60)).padStart(2, "0")
